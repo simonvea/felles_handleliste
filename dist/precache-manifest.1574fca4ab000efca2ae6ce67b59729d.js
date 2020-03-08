a48c506f17b588008b537c9ab99ecc0f446220a9
Simon Opheim <simon.opheim@attentec.no>
2020-03-08 14:36:51 +0100

    Initial dist subtree commit

---
+++ dist/precache-manifest.1574fca4ab000efca2ae6ce67b59729d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83f572a6432d28bee2a",
+    "url": "/felles_handleliste/css/app.1c8e6dae.css"
+  },
+  {
+    "revision": "2c627f419b9ea181be39",
+    "url": "/felles_handleliste/css/chunk-vendors.391ed6f9.css"
+  },
+  {
+    "revision": "b675e79a3124c85bc8dbb96f4dfca05e",
+    "url": "/felles_handleliste/index.html"
+  },
+  {
+    "revision": "9d3033e1e8f787668ffe",
+    "url": "/felles_handleliste/js/about.de20d5f8.js"
+  },
+  {
+    "revision": "b83f572a6432d28bee2a",
+    "url": "/felles_handleliste/js/app.8f82e4f2.js"
+  },
+  {
+    "revision": "2c627f419b9ea181be39",
+    "url": "/felles_handleliste/js/chunk-vendors.0f8b5301.js"
+  },
+  {
+    "revision": "ac001047e30f289851ec4295314559a4",
+    "url": "/felles_handleliste/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/felles_handleliste/robots.txt"
+  }
+]);
